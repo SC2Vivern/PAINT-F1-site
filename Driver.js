14e7a36d1f0402f46fedc624d1f10851446ce4d3
SC2Vivern <84450651+SC2Vivern@users.noreply.github.com>
2022-06-01 15:16:39 +0200

    Add bold to driver category names

--- Driver.js
+++ Driver.js
@@ -15,11 +15,11 @@ fetch(newUrl, requestOptions)
     var myObj = downloadedDoc.MRData.DriverTable.Drivers[0]
 
     let texter = "<h1><a href=\"" + myObj.url + "\">" + myObj.givenName + " " + myObj.familyName + "</a></h1>"
-    texter += "<img src=\"img/" + myObj.code + ".jpg\" style=\"object-fit: contain; width: 200px; height: 300px\"> <br>" 
-    texter += "Date of Birth: " + myObj.dateOfBirth + "<br>"
-    texter += "Nationality: " + myObj.nationality + "<br>"
-    texter += "Driver number: " + myObj.permanentNumber + "<br>"
-    texter += "Driver abbreviation: " + myObj.code
+    texter += "<img src=\"img/" + myObj.code + ".jpg\" style=\"object-fit: contain; width: 200px; height: 300px\"> <br>"
+    texter += "<b>Date of Birth: </b>" + myObj.dateOfBirth + "<br>"
+    texter += "<b>Nationality: </b>" + myObj.nationality + "<br>"
+    texter += "<b>Driver number: </b>" + myObj.permanentNumber + "<br>"
+    texter += "<b>Driver abbreviation: </b>" + myObj.code
 
     info.innerHTML = texter
   })
